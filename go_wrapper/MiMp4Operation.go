2ec4933025c7c4db9b1d3d8b01b90a14d05797b6
mediaios <iosmediadev@gmail.com>
2024-06-28 17:42:44 +0800

    update

--- go_wrapper/MiMp4Operation.go
+++ go_wrapper/MiMp4Operation.go
@@ -1,34 +1,35 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"io"
 	"os/exec"
-	"time"
-
-	"bufio"
 	"agora.io/agoraservice"
+	"time"
 )
 
 func main() {
 	svcCfg := agoraservice.AgoraServiceConfig{
-		AppId: "20338919f2ca4af4b1d7ec23d8870b56",
+		AppId:"20338919f2ca4af4b1d7ec23d8870b56",
 	}
 	agoraservice.Init(&svcCfg)
 	conCfg := agoraservice.RtcConnectionConfig{
-		SubAudio:      false,
-		SubVideo:      false,
-		ClientRole:    1,
-		ChannelProfile: 1,
+		SubAudio:		false,
+		SubVideo:		false,
+		ClientRole:		1,
+		ChannelProfile:	1,
 	}
 	conSignal := make(chan struct{})
 	connHandler := agoraservice.RtcConnectionEventHandler{
 		OnConnected: func(con *agoraservice.RtcConnection, info *agoraservice.RtcConnectionInfo, reason int) {
+			// do something
 			fmt.Println("Connected")
 			conSignal <- struct{}{}
 		},
 		OnDisconnected: func(con *agoraservice.RtcConnection, info *agoraservice.RtcConnectionInfo, reason int) {
+			// do something
 			fmt.Println("Disconnected")
 		},
 		OnUserJoined: func(con *agoraservice.RtcConnection, uid string) {
@@ -57,7 +58,22 @@ func main() {
 	}
 	sender.SetSendBufferSize(1000)
 
+
+
 	inputFile := "./test_data/sony_640.mp4"
+
+	//// 启动FFmpeg进程以分离视频数据
+	//videoCmd := exec.Command("ffmpeg", "-i", inputFile, "-f", "rawvideo", "-pix_fmt", "yuv420p", "-")
+	//videoOut, err := videoCmd.StdoutPipe()
+	//if err != nil {
+	//	fmt.Printf("Error creating video output pipe: %v\n", err)
+	//	return
+	//}
+	//videoErr, err := videoCmd.StderrPipe()
+	//if err != nil {
+	//	fmt.Printf("Error creating video error pipe: %v\n", err)
+	//	return
+	//}
 
 	// 启动FFmpeg进程以分离音频数据
 	audioCmd := exec.Command("ffmpeg", "-i", inputFile, "-f", "s16le", "-acodec", "pcm_s16le", "-")
@@ -73,22 +89,40 @@ func main() {
 	}
 
 	// 启动FFmpeg进程
+	if err := videoCmd.Start(); err != nil {
+		fmt.Printf("Error starting video FFmpeg process: %v\n", err)
+		return
+	}
 	if err := audioCmd.Start(); err != nil {
 		fmt.Printf("Error starting audio FFmpeg process: %v\n", err)
 		return
 	}
 
+	//// 处理视频数据
+	//go func() {
+	//	frameSize := 640 * 360 * 3 / 2 // 假设 YUV420P 格式和 1080p 分辨率
+	//	videoReader := bufio.NewReader(videoOut)
+	//	for {
+	//		buf := make([]byte, frameSize)
+	//		_, err := io.ReadFull(videoReader, buf)
+	//		if err != nil {
+	//			if err == io.EOF {
+	//				fmt.Println("Video data read complete")
+	//			} else {
+	//				fmt.Printf("Error reading video data: %v\n", err)
+	//			}
+	//			break
+	//		}
+	//		// 处理一帧视频数据
+	//		handleVideoFrame(buf)
+	//	}
+	//}()
+
 	// 处理音频数据
 	go func() {
+		//buf := make([]byte, 4096) // 根据需要调整缓冲区大小
 		audioReader := bufio.NewReader(audioOut)
-		defer func() {
-			sender.Stop()
-			con.Disconnect()
-			agoraservice.Destroy()
-		}()
-
 		for {
-			// 读取音频数据
 			dataLen, err := audioReader.Read(audioFrame.Data)
 			if err != nil {
 				if err == io.EOF {
@@ -98,32 +132,51 @@ func main() {
 				}
 				break
 			}
-
+		
 			// 检查读取的数据长度是否符合预期
+			fmt.Println("dataLen: %d",dataLen)
 			if dataLen < len(audioFrame.Data) {
-				fmt.Println("Incomplete audio frame")
-				break
+				fmt.Println("Incomplete audio frame, filling with silence")
+				for i := dataLen; i < len(audioFrame.Data); i++ {
+					audioFrame.Data[i] = 0 // 填充静音
+				}
 			}
-
-			// 发送音频数据给Agora SDK
-			sender.SendPcmData(&audioFrame)
-
-			// 控制音频发送频率，每隔10毫秒发送一次
+		
+			sender.SendPcmData(&audioFrame);
 			time.Sleep(10 * time.Millisecond)
+		
+			//handleAudioFrame(frame.Data[:n], sender, frame)
 		}
 	}()
 
-	// 打印 FFmpeg 的错误输出
+	// 读取并打印 FFmpeg 的错误输出
 	go func() {
+		videoErrReader := bufio.NewReader(videoErr)
 		audioErrReader := bufio.NewReader(audioErr)
+		printFFmpegErrorOutput(videoErrReader)
 		printFFmpegErrorOutput(audioErrReader)
 	}()
 
 	// 等待 FFmpeg 进程完成
+	if err := videoCmd.Wait(); err != nil {
+		fmt.Printf("Video FFmpeg process finished with error: %v\n", err)
+	}
 	if err := audioCmd.Wait(); err != nil {
 		fmt.Printf("Audio FFmpeg process finished with error: %v\n", err)
+
+		sender.Stop()
+		con.Disconnect()
+
+		agoraservice.Destroy()
 	}
 }
+
+// 处理视频帧的回调函数
+func handleVideoFrame(frame []byte) {
+	// 在这里处理每一帧视频数据
+	//fmt.Println("Received a video frame")
+}
+
 
 // 打印 FFmpeg 的错误输出
 func printFFmpegErrorOutput(reader *bufio.Reader) {
